7aeadcd1cb23b620cd3a09b93512536f4f00eb20
dylncheng <68773823+dylncheng@users.noreply.github.com>
2021-08-04 21:48:52 -0700

    big patch: temp and country select

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 import requests
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -11,23 +12,44 @@ OW_ENDPOINT_1 = 'https://api.openweathermap.org/data/2.5/weather'
 OW_ENDPOINT_2 = 'https://api.openweathermap.org/data/2.5/onecall'
 OW_API_KEY = '1b51a0d76b8c833467e07f4b689bee53'
 
+COUNTRY = 'Canada'
+
+
+country_dict = {}
+
+def get_countries():
+    global country_dict
+    with open('static/data/countries.csv', encoding='utf-8') as data_file:
+        data_list = data_file.readlines()
+        for i in range(len(data_list)):
+            data_list[i] = data_list[i].strip('\n')
+
+        for line in data_list[1:]:
+            country_list = line.split(',')
+            country_dict[country_list[0]] = country_list[1]
+
 @app.route('/', methods=["GET", "POST"])
 def home():
+    global COUNTRY
+    get_countries()
     if request.method == "GET":
         error = request.args.get('error', default=None, type=None)
         if not error:
             error = ""
-
-        return render_template('index.html', error=error)
+        return render_template('index.html', error=error, countries=country_dict.keys(), default=COUNTRY)
     else:
-        return redirect(url_for('get_weather', city=request.form['city']))
+        COUNTRY = request.form['country']
+        return redirect(url_for('get_weather', city=request.form['city'], country=COUNTRY))
+
+
 
 
 @app.route('/weather')
 def get_weather():
     city = request.args.get('city')
+    country = request.args.get('country')
     params_1 = {
-        'q': f"{city},CA",
+        'q': f"{city},{country_dict[country]}",
         'appid': OW_API_KEY,
         'units': 'metric'
     }
@@ -38,7 +60,7 @@ def get_weather():
         lat = city_data['coord']['lat']
         lon = city_data['coord']['lon']
     except KeyError:
-        return redirect(url_for('home', error="Invalid City: Please try again."))
+        return redirect(url_for('home', error="Invalid City: Please try again.", country=COUNTRY))
     # print(lat)
     # print(lon)
 
